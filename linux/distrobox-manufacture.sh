06cea702b82fbd47e1c3e12d2661a3e4a1a49f66
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-10-09 21:29:34 +0530

    fzf distrobox-manufacture

--- linux/distrobox-manufacture.sh
+++ linux/distrobox-manufacture.sh
@@ -1,38 +1,80 @@
 #!/bin/sh
 
+dependency_check() {
+
+if ! command -v fzf ; then
+	printf "\033[1m\033[38;5;1m[Error]\033[m fzf not found \n"
+	exit
+fi
+
+}
+
+
 db_create() {
 
-  db_name="$1"
-  db_image="$2"
-  db_home_dir=""
-  nvidia_flag=""
+  local db_name=""
+  local db_image=""
+  local db_home_dir=""
+  local nvidia_flag=""
+  local rootful_container_flag=""
+  local separate_initsystem_flag=""
 
-  printf "Name of distrobox (default:${db_name}) : " 
+  local db_image_selected="$(distrobox create --compatibility |  fzf -i --prompt "Select Image > " )"
+
+  if [ -n "${db_image_selected}" ]; then
+    db_image="${db_image_selected}"
+    printf "Using image : ${db_image} \n"
+  else
+	printf "No option selected \n"
+	exit 1
+  fi
+
+
+  printf "Name of distrobox : "
+  local _db_name
   read _db_name
 
   if [ -n "$_db_name" ]; then
-    echo "Setting distrobox name as : ${_db_name}"
     db_name="${_db_name}"
-  else 
-    echo "Using default distrobox name : ${db_name}"
+    printf "Setting distrobox name as : ${db_name} \n"
+  else
+    printf "Name not provided \n"
+    exit 1
   fi
 
-  printf "Do you want to create a separate home directory (y/n) (default:y) : "
+  printf "Do you want to create a separate home directory (Y/n) : "
+  local _separate_home_dir_confirm
   read _separate_home_dir_confirm
 
   case "$_separate_home_dir_confirm" in
     (n* | N*)
-      db_home_dir="$HOME" 
+      db_home_dir="$HOME"
       printf "Using host's home directory : ${db_home_dir} \n"
       ;;
     (y* | Y* | *)
       db_home_dir="$HOME/distroboxes/${db_name}"
-      printf "Created separate home directory at : ${db_home_dir} \n"
-      mkdir -p "${db_home_dir}"
+      printf "Create separate home directory at : ${db_home_dir} \n"
       ;;
   esac
 
-  printf "Do you want to enable nvidia support (y/n) (default:n) : "
+  printf "Do you want this container to have a separate init system (y/N) : "
+  local separate_initsystem_flag_confirm
+  read separate_initsystem_flag_confirm
+
+  case "$separate_initsystem_flag_confirm" in
+    (y* | Y*)
+      printf "Adding separate init system flag \n"
+      separate_initsystem_flag="--init"
+      ;;
+    (n* | N* | *)
+      printf "Not adding separate init system flag \n"
+      separate_initsystem_flag=""
+      ;;
+  esac
+
+
+  printf "Do you want to enable nvidia support (y/N) : "
+  local _nvidia_flag_confirm
   read _nvidia_flag_confirm
 
   case "$_nvidia_flag_confirm" in
@@ -46,79 +88,58 @@ db_create() {
       ;;
   esac
 
+
+  printf "Do you want this to be a rootful container (y/N) : "
+  local rootful_container_flag_confirm
+  read rootful_container_flag_confirm
+
+  case "$rootful_container_flag_confirm" in
+    (y* | Y*)
+      printf "Adding rootful container flag \n"
+      rootful_container_flag="--root"
+      ;;
+    (n* | N* | *)
+      printf "Not adding rootful container flag \n"
+      rootful_container_flag=""
+      ;;
+  esac
+
+
+
   printf "If you want to pass any custom flags type them here (default: "") : "
+  local custom_flags
   read custom_flags
-  
+
   if [ -n "$custom_flags" ]; then
     printf "Using custom flags : ${custom_flags} \n"
   else
     printf "Proceeding without any custom options \n"
   fi
-  
 
-  distrobox-create --name "${db_name}" --image "${db_image}"  --home "${db_home_dir}" ${nvidia_flag} ${custom_flags}
-  distrobox-enter "${db_name}"
+  rmdir --ignore-fail-on-non-empty  -- ~/distroboxes/*
+  mkdir -p "${db_home_dir}"
+  distrobox-create --name "${db_name}" --image "${db_image}"  --home "${db_home_dir}" ${separate_initsystem_flag} ${nvidia_flag} ${rootful_container_flag}  ${custom_flags}
+  distrobox-enter ${rootful_container_flag}  "${db_name}"
 }
 
 main() {
-echo "Distrobox Manufacture"
+printf "Distrobox Manufacture \n"
 
-echo "1. Debian 12"
-echo "2. Arch Linux"
-echo "3. Ubuntu 22.04"
-echo "4. Kali Linux"
-echo "5. OpenSUSE"
-echo "6. Alpine edge"
-
-printf "Choose your distro : "
-read _option
-
-case "${_option}" in 
-  "1")
-    echo "Selected Debian 12"
-    db_create "debian12" "quay.io/toolbx-images/debian-toolbox:12"
-    ;;
-  "2")
-    echo "Selected Arch Linux"
-    db_create "archlinux" "quay.io/toolbx-images/archlinux-toolbox"
-    ;;
-  "3")
-    echo "Selected Ubuntu 22.04 lts"
-    db_create "ubuntu22" "quay.io/toolbx-images/ubuntu-toolbox:22.04"
-    ;;
-  "4")
-    echo "Selected Kali Linux"
-    db_create "kali" "docker.io/kalilinux/kali-rolling:latest"
-    ;;
-  "5")
-    echo "Selected OpenSUSE"
-    db_create "opensuse" "registry.opensuse.org/opensuse/distrobox:latest"
-    ;;
-  "6")
-    echo "Selected Alpine edge"
-    db_create "alpine-edge" "quay.io/toolbx-images/alpine-toolbox:edge" 
-    ;;
-  *)
-    echo "Selected Unknown option"
-    ;;
-esac
-
-
+dependency_check
+db_create
 
 }
-
 
 main "$@"
 
 
-
-
+#     db_create "debian12" "quay.io/toolbx-images/debian-toolbox:12"
+#     db_create "archlinux" "quay.io/toolbx-images/archlinux-toolbox"
+#     db_create "ubuntu22" "quay.io/toolbx-images/ubuntu-toolbox:22.04"
+#     db_create "kali" "docker.io/kalilinux/kali-rolling:latest"
+#     db_create "opensuse" "registry.opensuse.org/opensuse/distrobox:latest"
+#     db_create "alpine-edge" "quay.io/toolbx-images/alpine-toolbox:edge"
 # distrobox create --name archlinux-init -i  docker.io/library/archlinux:latest --home $HOME/distroboxes/archlinux-init --init --init-hooks "umount /var/lib/flatpak"
 # distrobox create --root --name archlinux-init-root --image quay.io/toolbx-images/archlinux-toolbox --home $HOME/distroboxes/archlinux-init-root --init --init-hooks "umount /var/lib/flatpak"
-
 # distrobox-create --name fedora38-flatpak --image registry.fedoraproject.org/fedora-toolbox:38 --home $HOME/distroboxes/fedora38-flatpak --init-hooks "umount /var/lib/flatpak"
-
-
-
-
 
